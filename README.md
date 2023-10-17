5646f720f77b9cfb0296679d1517f3d56357f083
Priscilla Trevizan <priscillatrevizan.dev@gmail.com>
2023-10-16 21:44:31 -0300

    add readme

--- README.md
+++ README.md
@@ -1,2 +1,63 @@
-# Calculadora-Meta-Finandeira
-Projeto de Curso Match IBM - Calculadora de Meta Financeira com Economia Mensal
+
+# Projeto Final: Conclusão da Trilha de Desenvolvimento Web
+
+### Desafio  Final - PROJETO 06 Calculadora de Meta Financeira com Economia Mensal [Match_IBM!]
+
+
+Este projeto é uma calculadora em Python que permite aos usuários definirem uma meta financeira e calcular quanto tempo levará para atingi-la, com base em uma economia mensal definida. Essa aplicação é uma ferramenta valiosa para ajudar as pessoas a alcançarem seus objetivos financeiros, seja para uma viagem especial, uma compra importante ou qualquer outro objetivo financeiro.  
+
+## Funcionalidades
+
+#### Definição da Meta Financeira
+Os usuários podem inserir o valor da meta financeira que desejam atingir, representando o montante que desejam economizar para seu objetivo financeiro.
+
+#### Definição da Economia Mensal
+Os usuários fornecem o valor que podem economizar mensalmente, representando a quantia que estão dispostos a reservar regularmente para alcançar sua meta.
+
+#### Cálculo do Tempo Necessário
+A aplicação realiza cálculos com base na economia mensal e no valor da meta para determinar o número de meses necessários para atingir a meta financeira.
+
+#### Apresentação dos Resultados
+Os resultados são apresentados aos usuários de forma clara e concisa, exibindo o tempo estimado necessário para atingir a meta financeira com base nas informações fornecidas. Se o resultado for um número decimal, a aplicação informa ao usuário que é uma estimativa aproximada do tempo necessário.
+
+
+## Como Utilizar
+
+- Baixe o Projeto
+- Clone ou faça o download deste repositório para a sua máquina local
+- Instale as Dependências
+- Certifique-se de que você tenha Python instalado na sua máquina.
+
+A aplicação usa o pacote Tkinter para a interface gráfica, que já está incluído na maioria das distribuições Python. Portanto, normalmente, você não precisa instalar nada adicional.
+
+- Execute o Projeto
+- Navegue até o diretório do projeto no seu terminal.
+- Execute o arquivo Python myapp.py.
+
+### Utilize a Calculadora de Meta Financeira
+
+Insira o valor da sua meta financeira desejada.
+Forneça o valor que você pode economizar mensalmente.
+Clique no botão "Calcular" para obter o tempo estimado necessário para atingir sua meta financeira.
+Os resultados serão exibidos na tela.
+
+
+
+
+## Próximos Passos
+Os próximos passos para este projeto podem incluir:
+
+- Revisão e Aperfeiçoamento
+
+- Solicitar Feedback
+
+- Atualização Contínua
+
+## Conclusão
+Este projeto é um exercício do programa de treinamento em programação. Contribuições externas não são aceitas.
+
+## Agradecimentos à:
+
+ - [Match!](https://match.mastertech.com.br/)
+ - [IBM](www.ibm.com)
+ - [IBM SkillsBuild](https://skillsbuild.org/)
--- myapp.py
+++ myapp.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import font as tkFont
 
 
 def calcular_tempo():
@@ -8,48 +9,59 @@ def calcular_tempo():
 
         if economia_mensal <= 0:
             resultado_label.config(
-                text="A economia mensal deve ser maior que zero.")
+                text="A economia mensal deve ser maior que zero")
             return
 
-        tempo = meta_financeira / economia_mensal
-        resultado_label.config(
-            text=f"Tempo estimado para atingir a meta: {tempo:.2f} meses")
+        tempo = round(meta_financeira / economia_mensal)
+        if tempo == int(tempo):
+            resultado_label.config(
+                text=f"Tempo estimado para atingir a meta: {int(tempo)} meses")
+        else:
+            resultado_label.config(
+                text=f"Aproximadamente {int(tempo)} meses")
     except ValueError:
         resultado_label.config(
-            text="Certifique-se de inserir valores válidos.")
+            text="Certifique-se de inserir valores válidos")
 
 
 def criar_interface():
     janela = tk.Tk()
     janela.title("Calculadora de Meta Financeira")
-    janela.geometry("300x200")
+    janela.geometry("600x400")
+
+    montserrat_font = tkFont.Font(family="Montserrat", size=14)
 
     global meta_financeira_entry, economia_mensal_entry, resultado_label
 
-    meta_financeira_entry = criar_entrada(janela, "Meta Financeira:")
-    economia_mensal_entry = criar_entrada(janela, "Economia Mensal:")
-    criar_botao(janela, "Calcular", calcular_tempo)
-    resultado_label = criar_etiqueta(janela)
+    meta_financeira_entry = criar_entrada(
+        janela, "Meta Financeira:", montserrat_font)
+    economia_mensal_entry = criar_entrada(
+        janela, "Economia Mensal:", montserrat_font)
+    criar_botao(janela, "Calcular", calcular_tempo, montserrat_font)
+    resultado_label = criar_etiqueta(janela, montserrat_font)
 
     janela.mainloop()
 
 
-def criar_entrada(janela, texto):
-    label = tk.Label(janela, text=texto)
+def criar_entrada(janela, texto, fonte):
+    label = tk.Label(janela, text=texto, font=fonte,
+                     bg="#F2F2F2", fg="#163E73")
     label.pack()
     entrada_variavel = tk.StringVar()
-    entrada = tk.Entry(janela, textvariable=entrada_variavel)
+    entrada = tk.Entry(janela, textvariable=entrada_variavel,
+                       font=fonte, justify="center")
     entrada.pack()
     return entrada_variavel
 
 
-def criar_botao(janela, texto, comando):
-    botao = tk.Button(janela, text=texto, command=comando)
+def criar_botao(janela, texto, comando, fonte):
+    botao = tk.Button(janela, text=texto, command=comando,
+                      font=fonte, bg="#163E73", fg="#F2F2F2", bd=12)
     botao.pack()
 
 
-def criar_etiqueta(janela):
-    etiqueta = tk.Label(janela, text="")
+def criar_etiqueta(janela, fonte):
+    etiqueta = tk.Label(janela, text="", font=fonte, bg="#F2F2F2")
     etiqueta.pack()
     return etiqueta
 
